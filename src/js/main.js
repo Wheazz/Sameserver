ba264bc34a183455ab2d73f890bce4efb7188b04
Wheazz <muhammadkhairil2006@gmail.com>
2025-04-02 19:17:14 +0800

    Add files via upload

---
+++ src/js/main.js
@@ -0,0 +1,19 @@
+alert("Howdy Partner!");
+const audio1 = document.getElementById('soundhe');
+const playandpausebutton = document.getElementById('firtsbutton');
+
+let isplaying = false
+
+function toggleplaypause(){
+ if (isplaying){
+        audio1.pause();
+        playandpausebutton.textContent= 'pause';
+    }
+    else{
+        
+        audio1.play();
+        playandpausebutton.textContent= 'playing';
+    }
+    isplaying=!isplaying;
+}
+playandpausebutton.addEventListener('click', toggleplaypause);
